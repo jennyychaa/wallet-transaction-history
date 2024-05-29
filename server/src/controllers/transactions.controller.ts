d627c5dcad694464d468e3a3e7b72df9ea98a685
JC <16579792+jennyychaa@users.noreply.github.com>
2024-05-29 19:00:49 -0400

    Added comments about future implementation for the transaction controller

--- server/src/controllers/transactions.controller.ts
+++ server/src/controllers/transactions.controller.ts
@@ -2,7 +2,7 @@ import Moralis from 'moralis'
 import { EvmChain } from '@moralisweb3/common-evm-utils'
 
 async function getTransactionHistory(address: string, cursor?: any) {
-  const transactions = await Moralis.EvmApi.wallets.getWalletHistory({
+  const transaction = await Moralis.EvmApi.wallets.getWalletHistory({
     address,
     cursor,
     chain: EvmChain.ETHEREUM,
@@ -10,7 +10,22 @@ async function getTransactionHistory(address: string, cursor?: any) {
     order: 'DESC',
   })
 
-  return { transactions }
+  /*
+   * @TODO Transform the data received from Moralis Wallet API
+   *
+   * TO-DO LIST:
+   * 1. Fetch X number of transaction logs from Moralis API but only send Y (e.g., 5, 10) to the client-side.
+   *
+   * The API results return information that currently isn't relevant to our application.
+   * We only want to send valid data related to ERC-20 token transfers so client-side doesn't have to deal with bloated data.
+   *
+   * Action Items:
+   * - Research best method to store and cache relevant transaction logs.
+   * - Transform the data and send what's necessary in the client-side. See transformTransactionsApiData() @app/utils.
+   */
+  return {
+    transaction,
+  }
 }
 
 export default {
