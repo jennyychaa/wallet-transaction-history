bfcf334174ce71f5b40825088cc04cda8eb3dda0
JC <16579792+jennyychaa@users.noreply.github.com>
2024-05-29 23:09:01 -0400

    Installed React Infinite Scroller; implemented TransactionLogs compoennt

--- app/src/utils/index.ts
+++ app/src/utils/index.ts
@@ -1,3 +1,10 @@
+import { Transactions } from '../components/TransactionLogs'
+
+import {
+  Transaction,
+  TokenTransfer,
+} from '../components/TransactionLogs/models'
+
 const months = [
   'January',
   'February',
@@ -26,4 +33,65 @@ const weekdays = [
 export const getFormattedDate = (fullDate: string) => {
   const date = new Date(fullDate)
   return `${weekdays[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+}
+
+/*
+ * @TODO Move the data transformation implementation to the server-side
+ *
+ * Due to the time constraints of this project, this has been implemented on the client-side.
+ * For additional details, see getTransactionHistory() in @server/controllers/transactions.controllers.ts.
+ */
+export const transformTransactionsApiData = (
+  apiResults: any,
+  transactionsData: Transactions | null,
+): Transactions => {
+  // First, filter out the unnecessary data from the apiResults by checking:
+  // - is it a spam transaction?
+  // - is it a ERC-20 token transfer?
+  const filteredApiResults = apiResults.filter(
+    (result: { erc20_transfers: any[]; possible_spam: boolean }) =>
+      !result.possible_spam && result.erc20_transfers.length > 0,
+  )
+
+  const transactionHistory: Transactions = transactionsData
+    ? { ...transactionsData }
+    : {}
+  // Group the ERC-20 token transfers by date and store releveant transaction data.
+  for (let i = 0; i < filteredApiResults.length; i++) {
+    const { block_hash, block_timestamp, category, erc20_transfers, summary } =
+      filteredApiResults[i]
+    const [transactionDate, _] = block_timestamp.split('T')
+
+    let transfers: TokenTransfer[] = []
+
+    for (let j = 0; j < erc20_transfers.length; j++) {
+      const { token_symbol, address, direction, value_formatted } =
+        erc20_transfers[j]
+      transfers.push({
+        address,
+        symbol: token_symbol,
+        direction,
+        value: value_formatted,
+      })
+    }
+
+    const newTransactions: Transaction = {
+      hash: block_hash,
+      category,
+      summary,
+      timestamp: block_timestamp,
+      transfers,
+    }
+
+    if (transactionHistory[transactionDate]) {
+      transactionHistory[transactionDate] = [
+        ...transactionHistory[transactionDate],
+        newTransactions,
+      ]
+    } else {
+      transactionHistory[transactionDate] = [newTransactions]
+    }
+  }
+
+  return transactionHistory
 }
