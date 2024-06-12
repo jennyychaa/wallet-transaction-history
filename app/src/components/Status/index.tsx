ca9c1b8f20ba12094d7ee3e8b623e9cd854abcfa
JC <16579792+jennyychaa@users.noreply.github.com>
2024-06-12 19:48:44 -0400

    Fixed bugs

--- app/src/components/Status/index.tsx
+++ app/src/components/Status/index.tsx
@@ -4,6 +4,14 @@ import ExclamationIcon from '../../assets/exclamation.svg?react'
 import SuccessIcon from '../../assets/success.svg?react'
 import Loader from '../Loader'
 
+/*
+ * Assumptions: StatusType
+ * idle: waiting to fetch transaction data.
+ *  - instead of success, this state is used after successfully fetching transaction data when there are additional data to fetch.
+ * loading: in the process of fetching transaction data.
+ * success: successfully fetched transaction data and there are no additional data to fetch.
+ * error: network error in fetching the transaction data (e.g., 404, 500, etc).
+ */
 export type StatusType = 'idle' | 'loading' | 'success' | 'error'
 
 interface StatusProps {
@@ -47,7 +55,7 @@ const Status = ({ type }: StatusProps) => {
       <ErrorStatusMessage>
         <ExclamationIcon />
         <span>
-          Oops, there was an error loading more transactions.{' '}
+          Oops, there was an error loading transactions.{' '}
           <u>
             <strong>Try again</strong>
           </u>
